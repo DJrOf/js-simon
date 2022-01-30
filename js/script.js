7245c989bace1c255f18a8e31b19c18b2e2c71f6
DJrOf <danieljrofosu@gmail.com>
2022-01-30 23:51:37 +0100

    rndm-no-gen+timeout-userguess

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+
+/* 
+1) Generare 5 numeri casuali e mostrarli in un alert all'utente.
+2) Dall'ok  parte un timer di 30 secondi. - NB: L'alert blocca il flusso. il timer partirà a contare dopo che avete schiacciato ok. -
+3) Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite prompt().
+4) Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri  sono stati indovinati dall'utente.
+ */
+
+// 1) Random Unique Numbers generator
+const getRandomUniqueNumbers = (min, max, tot) => {
+    const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min;
+
+    const numbers = [];
+    while (numbers.lenght < tot) {
+        const randomNumber = getRandomNumber(min, max);
+    } 
+    return numbers;
+};
+
+const min = 1;
+const max = 100;
+const total = 5;
+
+
+const numbers = getRandomNumbers(min, max, total);
+
+console.log(numbers);
+
+alert(numbers);
+
+// 2
+
+setTimeout(() => { 
+    const userNumbers = [];
+    const correctNumbers = [];
+
+    while (userNumbers.lenght < total) {
+        let userGuess;
+        while (!isValidNumber(userGuess, min, max)) {
+            userGuess = parseInt(prompt(`ìnserisci un numero da ${min} a ${max}`)).trim();
+        }
+
+        if(userNumbers.includes(userGuess)) {
+            alert('Already choosen');
+        } else {
+            userNumbers.push(userGuess);
+            if (numbers.includes(userGuess)) correctNumbers.push(userGuess);
+        }
+    }
+
+    let message= 'No matches';
+
+    if (correctNumbers.lenght) {
+        message = `Hai indovinato ${correctNumbers.length} numeri: ${correcttNumbers}`;
+    }
+
+    alert(message);
+}, 30000);
